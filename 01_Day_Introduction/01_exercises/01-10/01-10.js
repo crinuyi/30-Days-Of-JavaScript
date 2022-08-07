8e91aeab0dcf37ee3e0a11d435550fd7e081aa25
crin <crinuyi@gmail.com>
2022-08-07 20:47:27 +0200

    day 1 introductory exercises and 01-10 exercises

---
+++ 01_Day_Introduction/01_exercises/01-10/01-10.js
@@ -0,0 +1,48 @@
+// ----- 01
+//comments can make code readable
+
+// ----- 02
+// Welcome to 30DaysOfJavaScript
+
+// ----- 03
+/* comments can make code readable, easy to reuse and informative */
+
+// ----- 04
+// variable.js
+let a = "test"
+let b = true
+let c = undefined
+let d = null
+
+// ----- 05
+// datatypes.js
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+
+// ----- 06
+let aa, ab, ac, ad, ae
+
+// ----- 07
+let ba = 4
+let bb = 5.2453
+let bc = 'sample text'
+let bd = true
+let be = null
+
+// ----- 08
+let firstName = 'Sample Firstname'
+let lastName = 'Sample Lastname'
+let maritialStatus = 'taken'
+let country = 'France'
+let age = 65
+
+// ----- 09
+let firstName2 = 'Sample Firstname', lastName2 = 'Sample Lastname', maritialStatus2 = 'taken', country2 = 'France', age2 = 65
+
+// ----- 10
+let myAge = 76
+let yourAge = 45
+console.log("I am", myAge, "years old.")
+console.log("You are", yourAge, "years old.")
